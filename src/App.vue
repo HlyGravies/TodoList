8f73a3a85ba56a99c3374c6fc0e0b78f1766138a
HlyGravies <nguyenhoang887984@gmail.com>
2024-05-28 16:03:33 +0900

    Finish

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <script setup>
-import{ ref, onMounted, computed, watch } from "vue";
+import { ref, onMounted, computed, watch } from "vue";
 const todos = ref([])
 const name = ref("")
-const inputContent = ref ("")
+const input_content = ref("")
 const input_category = ref(null)
 
-const todo_asc = computed(() => todos.value.sort((a, b) =>{
+const todo_asc = computed(() => todos.value.sort((a, b) => {
   return b.createdAt - a.createdAt
 }))
 
+const addTodo = () => {
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
+  })
+
+  input_content.value = ''
+  input_category.value = null
+
+}
+
+const removeTodo = todo =>{
+  todos.value = todo.value.filter(t => t !== todo)
+}
+
+watch(todos, newVal => {
+  localStorage.setItem("todos", JSON.stringify(newVal))
+}, { deep: true })
+
 // Save your name into the local storage and use it
-watch(name, (newVal) =>{
+watch(name, (newVal) => {
   localStorage.setItem('name', newVal)
 })
-onMounted(() =>{
+onMounted(() => {
   name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem("todos")) || []
 })
 </script>
 
@@ -23,14 +49,56 @@ onMounted(() =>{
   <main class="app">
     <section class="greeting">
       <h2 class="title">
-        What's up, <input type="text" placeholder="Name here" v-model="name"/>
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
       </h2>
     </section>
 
     <section class="create-todo">
       <h3>CREATE A TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What is on your todo list?</h4>
+        <input type="text" placeholder="e.g. Clean the house" v-model="input_content" />
+
+        <h4>Pick a category</h4>
+        <div class="options">
+
+          <label>
+            <input type="radio" name="category" value="business" v-model="input_category" />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input type="radio" name="category" value="personal" v-model="input_category" />
+            <span class="bubble business"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+        <input type="submit" value="Add todo">
+      </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${todo.category}`"></span>
+
+            <div class="todo-content">
+              <input type="text" v-model="todo.content">
+            </div>
+
+            <div class="action">
+              <button class="delete" click="removeTodo(todo)">Delete</button>
+            </div>
+          </label>
+
+        </div>
+      </div>
+
     </section>
   </main>
 
 </template>
-
